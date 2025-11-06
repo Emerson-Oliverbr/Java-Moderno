a3d8653d42aeef8f3cb0ca48c53958e069da72d2
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-11-06 11:02:38 -0300

    Iniciando Modulo 3 Controle de Fluxo

---
+++ Modulo-3 Controle de Fluxo/1.If-else/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		int a = 5;
+
+		if(a > 5) {
+			System.out.println("a é maior que 5? " + (a > 5));
+		}
+		else {
+			System.out.println("a é maior que 5? " + (a > 5));
+		}
+	}
+}
